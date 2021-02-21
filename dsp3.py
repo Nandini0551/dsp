cb28343258ee01dd4401289a8fe4d682c90190a9
Nandini0551 <79369096+Nandini0551@users.noreply.github.com>
2021-02-21 12:16:30 +0530

    Add files via upload

---
+++ dsp3.py
@@ -0,0 +1,23 @@
+import scipy.io
+from matplotlib import pyplot as plt
+from scipy.io import wavfile
+import numpy as np
+#y=10*np.random.rand(10)
+y=[1,2,1,2,3,4]
+z=np.zeros(len(y))
+M=int(input('enter order'))
+for n in range(0,len(y)):
+        s=0
+        q=[]
+        for k in range(0,M):
+               if n-k>=0:
+                     q.append(y[n-k])
+                     s=s+y[n-k]
+        z[n]=s/M
+print(y)
+print(z)
+plt.subplot(211)
+plt.stem(y)
+plt.subplot(212)
+plt.stem(z)
+plt.show()
